ac01d21ac4506e9d2a265fb42f0213401810723d
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2024-01-08 09:32:12 +0700

    @PathVariable demo

---
+++ src/main/java/com/example/basicspringbootandjsp_intellij/controller/PathVariableController.java
@@ -0,0 +1,25 @@
+package com.example.basicspringbootandjsp_intellij.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PathVariableController {
+
+    // http://localhost:8080/demoPathVariable/variable1/variable2
+
+    @RequestMapping("/demoPathVariable/{variable1}/{variable2}")
+    public String demoPathVariable(
+            @PathVariable(name = "variable1") String var1,
+            @PathVariable(name = "variable2") String var2,
+            ModelMap model
+    ) {
+        model.put("var1", var1);
+        model.put("var2", var2);
+
+        return "pathVariable";
+    }
+}
+
---
+++ src/main/webapp/WEB-INF/views/pathVariable.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: huynq
+  Date: 1/8/2024
+  Time: 08:50
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>@PathVariable demo</title>
+</head>
+<body>
+    <h2>Get value via @PathVariable: variable 1 = ${var1}</h2>
+    <h2>Get value via @PathVariable: variable 2 = ${var2}</h2>
+</body>
+</html>
