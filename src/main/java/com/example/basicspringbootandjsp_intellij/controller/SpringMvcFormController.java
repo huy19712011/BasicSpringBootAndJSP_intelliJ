b27167ae23c9e03c50aab7c16032b3c42e7b3009
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2024-01-12 07:29:56 +0700

    Spring MVC Form: data binding

---
+++ src/main/java/com/example/basicspringbootandjsp_intellij/controller/SpringMvcFormController.java
@@ -0,0 +1,36 @@
+package com.example.basicspringbootandjsp_intellij.controller;
+
+import com.example.basicspringbootandjsp_intellij.entity.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class SpringMvcFormController {
+
+    Map<Long, Employee> employeeMap = new HashMap<>();
+
+    @RequestMapping(value = "/employee", method = RequestMethod.GET)
+    public ModelAndView showForm() {
+        return new ModelAndView("employeeHome", "employee", new Employee(1000L, "Unknown", "123456"));
+    }
+
+
+    @RequestMapping(value = "/addEmployee", method = RequestMethod.POST)
+    public String submit(@ModelAttribute("employee") final Employee employee, final ModelMap model) {
+
+//        model.addAttribute("name", employee.getName());
+//        model.addAttribute("contactNumber", employee.getContactNumber());
+//        model.addAttribute("id", employee.getId());
+
+        employeeMap.put(employee.getId(), employee);
+        return "employeeView";
+    }
+}
